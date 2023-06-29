7d5962d6d35d02db63fa83528cba8e82096f532a
dbaoty <tcs9308@gmail.com>
2023-06-28 23:12:54 -0400

    add muting of previous audible tab ONLY when the new highlighted tab is also audible

--- src/background.js
+++ src/background.js
@@ -1,31 +1,41 @@
 /**
- * @todo Functionality: changes from audible to 
- * inaudible tabs do NOT mute the previous audible tab.
  * @todo Functionality: ability to toggle extension
  * on and off
- */
+ **/
 
 async function collectAudibleTabs() {
     const queryOpts = { audible: true };
     const tabs = await chrome.tabs.query(queryOpts);
 
-    console.log("DEBUG: Tab Mute Extension - detected " + tabs.length + " audible tabs.");
+    console.log("DEBUG: Tab Mute Extension - detected " +
+        tabs.length + " audible tabs.");
 
     return tabs;
 }
 
 async function toggleMute() {
-    const audibleTabs = await collectAudibleTabs();
+    /* get currently highlighted tab to determine audibility */
+    const currentTab = await chrome.tabs.query({ highlighted: true });
+    
+    /* if the currently highlight tab is not audible, do nothing.
+     * this is so going from an audible tab to an inaudible tab
+     * will not cause the previous tab to be muted. */
+    if (!currentTab[0].audible) return;
 
+    const audibleTabs = await collectAudibleTabs();
     for (let i = 0; i < audibleTabs.length; ++i) {
         const tabId = await audibleTabs[i].id;
         if (audibleTabs[i].active) {
             if (audibleTabs[i].mutedInfo.muted) {   
-                console.log("DEBUG: Tab Mute Extension - Tab ID: " + tabId + " UNMUTED");  
+                console.log("DEBUG: Tab Mute Extension - Tab ID: " + 
+                    tabId + " UNMUTED");
+
                 chrome.tabs.update(tabId, { muted: false });
             }
         } else {
-            console.log("DEBUG: Tab Mute Extension - Tab ID: " + tabId + " MUTED");
+            console.log("DEBUG: Tab Mute Extension - Tab ID: " +
+                tabId + " MUTED");
+
             chrome.tabs.update(tabId, { muted: true });
         }
     }
